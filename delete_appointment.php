6e87e221f6fc571ba0a6b8c78691d39f4bb660d5
divyanikhatri2007-code <your-email-used-for-this-github>
2026-02-02 16:14:25 +0545

    Initial commit of clinic app project

--- delete_appointment.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../auth/auth_check.php";
-require_once "../config/db.php";
-
-$stmt = $pdo->prepare("DELETE FROM appointments WHERE id=?");
-$stmt->execute([$_GET['id']]);
-
-header("Location: appointments.php");
